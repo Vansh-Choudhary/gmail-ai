d1e110c5697f81f396d99639d6cd863a2d15a63e
Vansh Choudhary <vanshchoudhary9405@gmail.com>
2024-06-08 16:59:26 +0530

    change

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,8 @@
 // pages/_app.js
-/*import '../styles/globals.css';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
 
 export default MyApp;
-*/
